960d3c9d3843831e1db131c17d8b90cda38797cb
Preome <tasdidanpreome@gmail.com>
2026-07-15 06:30:27 +0600

    fix

--- backend/ecommerce_backend/settings.py
+++ backend/ecommerce_backend/settings.py
@@ -108,10 +108,10 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'ecommerce_backend.exceptions.custom_exception_handler',
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+CORS_ALLOWED_ORIGINS = env.list(
+    'CORS_ALLOWED_ORIGINS',
+    default=['http://localhost:3000', 'http://127.0.0.1:3000']
+)
 
 CORS_ALLOW_CREDENTIALS = True
 
